d360a776703097a75b972e8372b1deeba2bbdb6c
Taehoon K <turtlescubing@gmail.com>
2018-08-11 21:59:46 -0400

    added simple math

--- main.js
+++ main.js
@@ -12,14 +12,54 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'boob') {
+  if (msg.slice(0,2) === '==') {
+    //determine where operator is
+    for (var i = 2; i < msg.content.length; i++) {
+      if (msg.content[i] === '+') {
+        var index = i;
+      }
+      else if (msg.content[i] === '-') {
+        var index = i;
+      }
+      else if (msg.content[i] === '*') {
+        var index = i;
+      }
+      else if (msg.content[i] === '/') {
+        var index = i;
+      }
+    }
+    //create numbers
+    var numA = parseFloat(msg.content.slice(2,index));
+    var numB = parseFloat(msg.content.slice(index+1));
+    //ensure that conversion worked
+    if (numA != NaN & numB != NaN) {
+      //carry out operation
+      var answer;
+      if (msg.content[index] === '+') {
+        answer = numA + numB;
+      }
+      else if (msg.content[index] === '-') {
+        answer = numA - numB;
+      }
+      else if (msg.content[index] === '*') {
+        answer = numA * numB;
+      }
+      else if (msg.content[index] === '/') {
+        answer = numA / numB;
+      }
+      //send message
+      msg.reply(msg.content + " " + answer.toString());
+    }
+  }
+
+  else if (msg.content === 'boob') {
     msg.reply('elbow');
   }
   else if (msg.content === 'elbow') {
     msg.reply('boob');
   }
 
-  if (msg.content === '!joke') {
+  else if (msg.content === '!joke') {
     //random joke
     //msg.reply(jokesList[Math.floor(Math.random()*jokesList.length)]);
     //ur mom
